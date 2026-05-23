6843dc0ae69d78bc668cac0b0fdae2badd760011
Dinesh Pandian G <146769255+Dinesh-max-code@users.noreply.github.com>
2026-05-23 20:55:48 +0530

    Initial CI/CD project setup

---
+++ src/main/java/aws_devops_cicd/AwsDevopsCicdApplication.java
@@ -0,0 +1,13 @@
+package aws_devops_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevopsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDevopsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aws_devops_cicd/AwsDevopsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package aws_devops_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDevopsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
