4e4d8b492024e8f7e0945aae54cc42cd1388d254
Dinesh Pandian G <146769255+Dinesh-max-code@users.noreply.github.com>
2026-05-24 18:54:10 +0530

    Change package declaration to aws_devops_cicd

--- src/main/java/aws_devops_cicd/HomeController.java
+++ src/main/java/aws_devops_cicd/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.aws_devops_cicd;
+package aws_devops_cicd;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
